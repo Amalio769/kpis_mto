f7ad58060dceef47fa8178ba5f2accc259de807a
Amalio769 <amalio.rete-campos@airbus.com>
2021-02-18 19:33:29 +0100

    Agrupamiento y pivot del resultado total del dataframe

--- kpis/tests/test_actualizar_total_costes.py
+++ kpis/tests/test_actualizar_total_costes.py
@@ -11,7 +11,7 @@ import kpis.configuracion.config as cfg
 import datetime
 import pandas as pd
 
-year = 2020
+year = 2021
 
 hr301 = 82.19
 hr211 = 70.00
@@ -39,6 +39,14 @@ df.rename(columns={'coste_total':'coste'}, inplace=True)
 df.rename(columns={'cl_po':'po_grafo'}, inplace=True)
 df.rename(columns={'fecha_pedido':'fecha'}, inplace=True)
 
+for idx in range(1,13):
+    linea_dummy = {'ceco':'', 'centro':'', 'po_grafo':'', 'coste_por_entregar':0,
+       'coste_por_facturar':0, 'coste':0, 'descripcion_material':'', 'pep':'', 'fecha':'',
+       'grupo_articulo':'', 'imputacion':'', 'material':'', 'mes':idx, 'nombre_proveedor':'',
+       'orden':'', 'pedido':'', 'pedido_pos':'', 'clase_coste':''}
+    df = df.append(linea_dummy, ignore_index=True)
+
+
 #datos.df_pco2excel_app(df,year)
 #datos.df_pco2excel_kpisites(df,year)
 
@@ -48,10 +56,25 @@ df1['coste']=df1[['ceco','horas']].apply(lambda x: x['horas']*hr301 if x['ceco']
 df1['clase_coste']="GRAFO"
 df1.rename(columns={'grafo':'po_grafo'}, inplace=True)
 
+
 df_total=pd.merge(df,df1, how='outer', on=['pep','clase_coste','po_grafo','coste','ceco','mes','fecha'])
+df_total.reset_index(drop = True, inplace = True)
+
+df_total_1 = df_total[['pep','ceco','clase_coste','po_grafo','mes','coste']]
+#df_total_1['coste'] = df_total_1['coste'].div(1000).round(2)
+df_total_1_gby = df_total_1.groupby(['pep','ceco','clase_coste','po_grafo','mes'], as_index=False).sum()
+pivot_total1=pd.pivot_table(df_total_1_gby,\
+                            index=['pep','ceco','clase_coste','po_grafo'],\
+                            columns='mes',\
+                            values='coste',\
+                            aggfunc=sum,\
+                            margins=False)
 
 with pd.ExcelWriter(cfg.PATH_COSTES_OUTPUT + 'Datos-PO-' + str(year) + '.xlsx') as output:
-    df_total.to_excel(output, sheet_name='total')
+    df_total.to_excel(output, sheet_name='total', index= False)
+    df_total_1.to_excel(output, sheet_name='total_1', index=False)
+    df_total_1_gby.to_excel(output, sheet_name='gby', index=False)
+    pivot_total1.to_excel(output, sheet_name='pivot', index=True)
 
 #datos.df_zpscapp2excel_app(df,year)
 #datos.df_zpscapp2excel_kpisites(df,year)
